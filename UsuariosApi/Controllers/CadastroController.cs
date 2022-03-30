aa38ebb3264bb40dc0071fc166ad3ef11d12e1ee
Adriano Gomes de Oliveira <oliveradriano.ads@gmail.com>
2022-03-29 22:00:12 -0300

    Criação de api usuarios para autenticação

---
+++ UsuariosApi/Controllers/CadastroController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+
+namespace UsuariosApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+        public ActionResult CadastrarUsuario(CreateUsuarioDto createDto)
+        {
+            return Ok();
+        }
+    }
+}
