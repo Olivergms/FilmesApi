9b8e97d9bf27e0882fa6014fb1cfa47489074281
Adriano Gomes de Oliveira <oliveradriano.ads@gmail.com>
2022-03-29 23:02:46 -0300

    Configurando Identity

---
+++ UsuariosApi/Data/UsuarioContexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UsuariosApi.Data
+{
+    //IdentitydbContext possui um identity user, identificado pelo id int
+    // um papel identificado por inteiro
+    // e será identificado por um inteiro
+    public class UsuarioContexto : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
+    {
+        public UsuarioContexto(DbContextOptions<UsuarioContexto> opt)
+            : base(opt) { }
+    }
+}
--- UsuariosApi/Startup.cs
+++ UsuariosApi/Startup.cs
@@ -1,16 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using UsuariosApi.Data;
 
 namespace UsuariosApi
 {
@@ -28,7 +25,21 @@ namespace UsuariosApi
         {
 
             services.AddControllers();
+            //registrando o automapper
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            //registrando db context
+            services.AddDbContext<UsuarioContexto>
+                (opt => opt.UseSqlServer
+                (Configuration
+                .GetConnectionString("UsuarioConnection"))
+            );
+
+            //registrando o identity
+            services.AddIdentity<IdentityUser<int>, IdentityRole<int>>()
+                //Irá usar a store para armazenar os dados
+                .AddEntityFrameworkStores<UsuarioContexto>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "UsuariosApi", Version = "v1" });
