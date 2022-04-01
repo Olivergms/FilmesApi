255fa51a13da16440bcfd586320268a1d0c7131b
Adriano Gomes de Oliveira <oliveradriano.ads@gmail.com>
2022-03-31 21:21:03 -0300

    Gerando token

---
+++ UsuariosApi/Services/TokenService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsuariosApi.Modelos;
+
+namespace UsuariosApi.Services
+{
+    public class TokenService
+    {
+        public Token CreateToken(IdentityUser<int> usuario)
+        {
+            Claim[] direitoUsuario = new Claim[]
+            {
+                new Claim("username", usuario.UserName),
+                new Claim("id", usuario.Id.ToString())
+            };
+            #region Gerando token
+            var chave = new SymmetricSecurityKey(
+                 Encoding.UTF8.GetBytes("AluraCursos"));
+
+            var credenciais = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: direitoUsuario,
+                signingCredentials: credenciais,
+                expires: DateTime.UtcNow.AddHours(1)
+                );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+            return new Token(tokenString);
+            #endregion
+
+        }
+    }
+}
